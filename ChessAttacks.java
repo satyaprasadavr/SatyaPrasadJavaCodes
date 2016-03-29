83a2b0cc34070a22a41968a41eef04c7823ac617
satyaprasadavr <satyaprasadavr@gmail.com>
2016-03-29 23:13:20 +0530

    King,Bishop,Elephant,Queen Attack checked
    King,Bishop,Elephant,Queen Attack checked

--- ChessAttacks.java
+++ ChessAttacks.java
@@ -1,5 +1,3 @@
-package com.talent;
-
 public class ChessAttacks {
 	final static String PLAYING = "BLACK"; // "WHITE"
 
@@ -11,6 +9,28 @@ public class ChessAttacks {
 		}
 	}
 
+	public static int isInLineOfElephant(int rowDiff, int colDiff) {
+		return rowDiff == 0 || colDiff == 0 ? 1 : 0;
+	}
+
+	public static int isInLineOfBishop(int rowDiff, int colDiff) {
+		return Math.abs(rowDiff) == Math.abs(colDiff) ? 1 : 0;
+	}
+
+	public static int isInLineOfKnight(int rowDiff, int colDiff) {
+		return (Math.abs(rowDiff) == 1 && Math.abs(colDiff) == 2)
+				|| (Math.abs(rowDiff) == 2 && Math.abs(colDiff) == 1) ? 1 : 0;
+	}
+
+	public static int isInLineOfKing(int rowDiff, int colDiff) {
+		return (Math.abs(rowDiff) <= 1 && Math.abs(colDiff) <= 1) ? 1 : 0;
+	}
+
+	public static int isInLineOfQueen(int rowDiff, int colDiff) {
+		return (isInLineOfBishop(rowDiff, colDiff) == 1 || isInLineOfElephant(
+				rowDiff, colDiff) == 1) ? 1 : 0;
+	}
+
 	public static int getColPosition(String position) {
 		return position.toUpperCase().charAt(0) - 64;
 	}
@@ -20,21 +40,43 @@ public class ChessAttacks {
 	}
 
 	public static int checkAttack(String coinOne, String coinPosition,
-			String coinOther,String otherPosition) {
-		int colPosPawn = getColPosition(coinPosition);
-		int rowPosPawn = getRowPosition(coinPosition);
+			String coinOther, String otherPosition) {
+		int colPosOne = getColPosition(coinPosition);
+		int rowPosOne = getRowPosition(coinPosition);
 		int colPosOther = getColPosition(otherPosition);
 		int rowPosOther = getRowPosition(otherPosition);
-		int colDiff = colPosOther - colPosPawn;
-		int rowDiff = rowPosPawn - rowPosOther;
+		int colDiff = colPosOne - colPosOther;
+		int rowDiff = rowPosOne - rowPosOther;
 		int sum = 0;
 		if (coinOne.toUpperCase().equals("PAWN")) {
 			sum += isInLineOfPawn(rowDiff, colDiff);
 		}
+		if (coinOne.toUpperCase().equals("ELEPHANT")) {
+			sum += isInLineOfElephant(rowDiff, colDiff);
+		}
+		if (coinOne.toUpperCase().equals("BISHOP")) {
+			sum += isInLineOfBishop(rowDiff, colDiff);
+		}
+		if (coinOne.toUpperCase().equals("KNIGHT")) {
+			sum += isInLineOfKnight(rowDiff, colDiff);
+		}
+		if (coinOne.toUpperCase().equals("KING")) {
+			sum += isInLineOfKing(rowDiff, colDiff);
+		}
+		if (coinOne.toUpperCase().equals("QUEEN")) {
+			sum += isInLineOfQueen(rowDiff, colDiff);
+		}
+
 		return sum;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(checkAttack("PAWN","A3","PAWN","A4"));
+		System.out.println(checkAttack("PAWN", "C5", "PAWN", "D4"));
+		System.out.println(checkAttack("ELEPHANT", "C5", "PAWN", "D5"));
+		System.out.println(checkAttack("BISHOP", "C5", "PAWN", "A3"));
+		System.out.println(checkAttack("KNIGHT", "C3", "PAWN", "B5"));
+		System.out.println(checkAttack("KING", "D3", "PAWN", "C2"));
+		System.out.println(checkAttack("QUEEN", "I1", "PAWN", "J1"));
+
 	}
 }
